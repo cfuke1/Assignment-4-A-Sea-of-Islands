df4fb619b1bba30f78778b6e422f81c0d790c433
cfuke1 <156386553+cfuke1@users.noreply.github.com>
2024-07-11 22:23:33 -1000

    commit
    commit

--- WeightedGraph.java
+++ WeightedGraph.java
@@ -1,65 +1,106 @@
-import java.util.LinkedList;
+import java.util.*;
 
-public class WeightedGraph {
+class WeightedGraph {
+    private final Map<String, Vertex> vertices;
+
+    static class Vertex {
+        String id;
+        int time;
+        List<Edge> edges;
+
+        Vertex(String id, int time) {
+            this.id = id;
+            this.time = time;
+            this.edges = new ArrayList<>();
+        }
+    }
+
     static class Edge {
-        int source;
-
-        int destination;
+        String target;
         int weight;
 
-        public Edge(int source, int destination, int weight) {
-            this.source = source;
-            this.destination = destination;
+        Edge(String target, int weight) {
+            this.target = target;
             this.weight = weight;
         }
     }
 
-    static class Node {
-        String island;
-        String resource;
-        int population;
+    public WeightedGraph() {
+        this.vertices = new HashMap<>();
     }
 
-    static class Graph {
-        int vertices;
-        LinkedList<Edge> [] adjacencylist;
+    public void addVertex(String id, int time) {
+        vertices.putIfAbsent(id, new Vertex(id, time));
+    }
 
-        Graph(int vertices) {
-            this.vertices = vertices;
-            adjacencylist = new LinkedList[vertices];
-            //initialize adjacency lists for all the vertices
-            for (int i = 0; i <vertices ; i++) {
-                adjacencylist[i] = new LinkedList<>();
-            }
+    public void addEdge(String sourceId, String targetId, int weight) {
+        Vertex source = vertices.get(sourceId);
+        Vertex target = vertices.get(targetId);
+        if (source != null && target != null) {
+            source.edges.add(new Edge(targetId, weight));
+            target.edges.add(new Edge(sourceId, weight)); // Undirected
         }
+    }
 
-        public void addEdge(int source, int destination, int weight) {
-            Edge edge = new Edge(source, destination, weight);
-            adjacencylist[source].addFirst(edge); //for directed graph
+    // Dijkstra's algorithm to find shortest paths from a source vertex
+    public Map<String, Integer> dijkstra(String sourceId) {
+        Map<String, Integer> distances = new HashMap<>();
+        PriorityQueue<Edge> priorityQueue = new PriorityQueue<>(Comparator.comparingInt(edge -> edge.weight));
+
+        for (String vertexId : vertices.keySet()) {
+            distances.put(vertexId, Integer.MAX_VALUE); // Set all distances to infinity
         }
+        distances.put(sourceId, 0); // Distance to the source is 0
+        priorityQueue.add(new Edge(sourceId, 0));
 
-        public void printGraph(){
-            for (int i = 0; i <vertices ; i++) {
-                LinkedList<Edge> list = adjacencylist[i];
-                for (int j = 0; j <list.size() ; j++) {
-                    System.out.println("vertex-" + i + " is connected to " +
-                            list.get(j).destination + " with weight " +  list.get(j).weight);
+        while (!priorityQueue.isEmpty()) {
+            Edge currentEdge = priorityQueue.poll();
+            String currentVertexId = currentEdge.target;
+
+            // Explore neighbors
+            for (Edge edge : vertices.get(currentVertexId).edges) {
+                int newDist = distances.get(currentVertexId) + edge.weight;
+                if (newDist < distances.get(edge.target)) {
+                    distances.put(edge.target, newDist);
+                    priorityQueue.add(new Edge(edge.target, newDist));
                 }
             }
         }
+
+        return distances;
     }
+
+    public void display() {
+        for (Vertex vertex : vertices.values()) {
+            System.out.print(vertex.id + " (" + vertex.time + ") -> ");
+            for (Edge edge : vertex.edges) {
+                System.out.print("(" + edge.target + ", " + edge.weight + ") ");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
-        int vertices = 6;
-        Graph graph = new Graph(vertices);
-        graph.addEdge(0, 1, 4);
-        graph.addEdge(0, 2, 3);
-        graph.addEdge(1, 3, 2);
-        graph.addEdge(1, 2, 5);
-        graph.addEdge(2, 3, 7);
-        graph.addEdge(3, 4, 2);
-        graph.addEdge(4, 0, 4);
-        graph.addEdge(4, 1, 4);
-        graph.addEdge(4, 5, 6);
-        graph.printGraph();
+        WeightedGraph graph = new WeightedGraph();
+        graph.addVertex("Hawaii", 10);
+        graph.addVertex("New Zealand", 20);
+        graph.addVertex("Easter Island", 30);
+        graph.addVertex("Tahiti", 40);
+        graph.addVertex("Samoa", 50);
+
+        graph.addEdge("Hawaii", "New Zealand", 5);
+        graph.addEdge("Hawaii", "Easter Island", 10);
+        graph.addEdge("New Zealand", "Tahiti", 3);
+        graph.addEdge("Easter Island", "Samoa", 1);
+        graph.addEdge("Tahiti", "Samoa", 8);
+
+        graph.display();
+
+        // Run Dijkstra's algorithm from Hawaii
+        Map<String, Integer> distances = graph.dijkstra("Hawaii");
+        System.out.println("Shortest distances from Hawaii:");
+        for (Map.Entry<String, Integer> entry : distances.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
     }
 }
